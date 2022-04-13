77ad687ba7675771eaba294054ba90f9b624b487
prymskiwen <hello.pronim@gmail.com>
2022-04-14 09:31:47 +1000

    feat: update StepIcon component

--- src/components/common/stepIcon/index.js
+++ src/components/common/stepIcon/index.js
@@ -1,10 +1,11 @@
 import styled from "styled-components/macro";
 
-const StepIconRoot = styled("div")(({ theme, ownerState }) => ({
-  backgroundImage:
-    ownerState.active || ownerState.completed
-      ? "url(static/img/step_icon_active.png)"
-      : "url(static/img/step_icon_inactive.png)",
+const StepIconRoot = styled("div")(({ ownerState }) => ({
+  backgroundImage: ownerState.active
+    ? "url(static/img/step_icon_active.png)"
+    : ownerState.completed
+    ? "url(static/img/step_icon_inactive.png)"
+    : "url(static/img/step_icon_disabled.png)",
   backgroundRepeat: "no-repeat",
   backgroundPosition: "center",
   backgroundSize: "contain",
@@ -12,6 +13,7 @@ const StepIconRoot = styled("div")(({ theme, ownerState }) => ({
   height: ownerState.active ? 50 : 40,
   borderRadius: "50%",
   zIndex: 1,
+  cursor: (ownerState.active || ownerState.completed) && "pointer",
 }));
 
 export default StepIconRoot;
